7fac3df48d5d453075648246ce6c51f992471ad7
ccsss <chansol0505@naver.com>
2021-04-19 22:34:04 +0900

    hash

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import glob
+from fnv64basedhash import *
+import hashlib
+
+def hash(w):
+    return hashlib.md5(w.encode('utf-8')).hexdigest()[:9]
 
 def create_def_link():
     for path in glob.glob('site/**/*.html', recursive=True):
@@ -8,9 +13,10 @@ def create_def_link():
 
         soup = BeautifulSoup(html, 'lxml')
 
-        for i, defs in enumerate(soup.select('div.tldr')):
-            defs['id'] = repr(i)
-            defs.wrap(soup.new_tag('a', href='#' + repr(i)))
+        for defs in soup.select('div.tldr'):
+            hashed = hash(defs.text)
+            defs['id'] = hashed
+            defs.wrap(soup.new_tag('a', href='#' + hashed))
 
         with open(path, 'w', encoding='utf-8') as f:
             f.write(str(soup))
